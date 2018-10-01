fc2d10d03b8ec46fe40ee5d13e98882ea316f097
Alexander Paz <alexjpaz@gmail.com>
2018-10-01 11:48:03 -0400

    Proxy stuff to alexjpaz.github.io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+var proxy = require('express-http-proxy');
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('*',proxy('https://alexjpaz.github.io/'));
 
 module.exports = app;
