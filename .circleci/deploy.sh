ddff69e4bee9d82cf5f7272d4a661f550e8e2526
Alexander Paz <alexjpaz@gmail.com>
2018-10-01 08:53:30 -0400

    logging

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,12 +7,15 @@ sls() {
 }
 
 sls_rollback() {
+    echo "=== Rolling back ${STAGE} to ${timestamp}  ==="
     local timestamp=$1
     sls rollback --timestamp=${timestamp}
     exit 1
 }
 
-sls deploy list --stage ${STAGE} -v | tee deploy-list.out
+echo "=== Deploying to ${STAGE} stage ==="
+
+sls deploy list --stage ${STAGE} -v &> tee deploy-list.out
 PREVIOUS_VERSION=$(cat deploy-list.out | grep 'Timestamp' | awk '{ print $3 }' | sort -nr)
 sls deploy --stage ${STAGE} -v | tee deploy.out
 BASE_URL=$(cat deploy.out | grep 'ServiceEndpoint' | awk '{ print $2 }') npm run test:integration
