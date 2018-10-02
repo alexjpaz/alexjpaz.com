53953726966629416102b7c8f44d10124a51796c
Alexander Paz <alexjpaz@gmail.com>
2018-10-01 20:15:53 -0400

    Remove app

--- test-integration/app.spec.js
+++ test-integration/app.spec.js
@@ -2,7 +2,7 @@ var fail = require("chai").assert.fail;
 
 const supertest = require('supertest');
 
-const request = supertest(process.env.BASE_URL || app);
+const request = supertest(process.env.BASE_URL);
 
 it('should invoke', async () => {
   await request.get('/hello')
