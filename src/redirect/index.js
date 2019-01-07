ddcfa2128fdb1f2da3fcb341e7eabc6b247fcbee
Alexander Paz <alexjpaz@gmail.com>
2019-01-07 15:50:02 -0500

    nani?

--- src/redirect/index.js
+++ src/redirect/index.js
@@ -10,4 +10,8 @@ module.exports = (app) => {
   app.get('/be-my-friend', (req, res) => {
     res.redirect('https://www.youtube.com/watch?v=1Uy8D_KsdyA');
   });
+
+  app.get('/nani', (req, res) => {
+    res.redirect('https://www.youtube.com/embed/vxKBHX9Datw?start=6&autoplay=1')
+  });
 };
