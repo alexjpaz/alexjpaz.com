c09df166352e8f97f5e0ee525afe973e9354e4c4
Alexander Paz <alexjpaz@gmail.com>
2019-02-01 13:45:47 -0500

    Adding expected endpoints test

--- test-integration/app.spec.js
+++ test-integration/app.spec.js
@@ -6,14 +6,24 @@ const app = require('../src');
 
 const request = supertest(process.env.BASE_URL || app);
 
-it('should invoke', async () => {
-  await request.get('/hello')
-    .expect(200, /Hello/);
-})
-
-describe('/wishlist', () => {
-  it('should invoke', async () => {
-    await request.get('/wishlist')
-      .expect(302);
-  })
+describe('expected endpoints', () => {
+  const endpoints = [
+    ['/hello', 200, /Hello/],
+    ['/wishlist', 302],
+    ['/and-his-name-is/foobar', 200, /foobar/]
+  ].forEach((endpoint) => {
+    const url = endpoint[0];
+    const statusCode = endpoint[1];
+    const resultMatch = endpoint[2];
+    const method = 'get'
+    it(`${method} ${url} = ${statusCode} ${resultMatch}`, async () => {
+      if(!resultMatch) {
+      await request[method](url)
+        .expect(statusCode)
+      } else {
+        await request[method](url)
+          .expect(statusCode, resultMatch)
+      }
+    });
+  });
 });
