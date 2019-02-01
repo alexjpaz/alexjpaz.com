efe4d54970537e9ec9a1273d025e9aa3fb0a5cb2
Alexander Paz <alexjpaz@gmail.com>
2019-02-01 13:13:15 -0500

    Adding "and his name is X <boooooom!>

---
+++ src/and-his-name-is/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+
+module.exports = (app, options) => {
+  app.get('/and-his-name-is/silence', (req, res) => {
+  });
+
+  app.get('/and-his-name-is/:name', async (req, res) => {
+    const name = req.params.name;
+
+    const html = await readFileAsync(path.join(__dirname, 'app.html'));
+
+    const result = html.toString()
+      .replace(/{{ name }}/g, name)
+  
+    res.send(result);
+  });
+};
