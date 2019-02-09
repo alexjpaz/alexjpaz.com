8a0f905f3e08ef9729839d63329a50ca9e3b741c
Alexander Paz <alexjpaz@gmail.com>
2019-02-09 08:35:21 -0500

    Do not show integration

--- test-integration/app.spec.js
+++ test-integration/app.spec.js
@@ -7,13 +7,14 @@ describe('expected endpoints', () => {
   const endpoints = [
     ['/hello', 200, /Hello/],
     ['/wishlist', 302],
-    ['/and-his-name-is/foobar', 200, /foobar/]
+    ['/and-his-name-is/foobar', 200, /foobar/],
+    ['/dist/and-his-name-is.js', 200]
   ].forEach((endpoint) => {
     const url = endpoint[0];
     const statusCode = endpoint[1];
     const resultMatch = endpoint[2];
     const method = 'get'
-    it(`${method} ${url} = ${statusCode} ${resultMatch}`, async () => {
+    it(`${method} ${url} = ${statusCode} ${resultMatch || ''}`, async () => {
       if(!resultMatch) {
       await request[method](url)
         .expect(statusCode)
