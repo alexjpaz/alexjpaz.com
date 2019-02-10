7d65223ea3b1e6274a4c377a73b4163934d294f1
Alexander Paz <alexjpaz@gmail.com>
2019-02-10 11:45:49 -0500

    doggo

--- src/doggo/index.js
+++ src/doggo/index.js
@@ -5,7 +5,28 @@ module.exports = (app, options) => {
    return images[Math.floor(Math.random()*images.length)];
   }
 
+  function getRandomIndex() {
+    return Math.floor(Math.random()*images.length);
+  }
+
   app.get('/doggo', (req, res) => {
+    let i = getRandomIndex();
+
+    if(req.query && req.query.i) {
+      i = req.query.i;
+    } else {
+      return res.redirect(`/doggo?i=${i}`);
+    }
+
+    const imageUrl = images[i];
+
+    return res.send(`
+    <html>
+      <img src='${imageUrl}' />
+    </html>
+    `);
+
     res.redirect(getRandomImage())
   });
+
 };
