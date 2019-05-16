a67eb1d16425cb1116b950c97a9c01fde024ec40
Alexander Paz <alexjpaz@gmail.com>
2019-05-16 14:38:01 -0400

    Add google analytics

--- packages/home-gatsby/gatsby-config.js
+++ packages/home-gatsby/gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@alexjpaz`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-25975548-2",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
