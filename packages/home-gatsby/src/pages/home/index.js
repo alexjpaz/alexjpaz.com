402cc44f181470998daf4ef4a3cb932629f2e54b
Alexander Paz <alexjpaz@gmail.com>
2019-05-19 19:34:04 -0400

    🙉

--- packages/home-gatsby/src/pages/home/index.js
+++ packages/home-gatsby/src/pages/home/index.js
@@ -92,7 +92,7 @@ const HomeFooter = () => (
       <p>
         <span>Built by </span><a href="https://twitter.com/alexjpaz">@alexjpaz</a>.
       </p>
-      <p>✌</p>
+      <p>🙉</p>
       <p><a href='https://www.gatsbyjs.org/'>Built with Gatsby</a></p>
       <p>© 2019</p>
     </div>
