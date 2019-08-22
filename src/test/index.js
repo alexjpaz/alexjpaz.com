a293f5e650c5db9b9bb7e2520f47ec6d928dc7ce
Alexander Paz <alexjpaz@users.noreply.github.com>
2019-08-22 15:51:41 -0400

    Add an html5 application builder (#6)
    * Add an html5 application builder
    
    * Adding noscript tag
    
    * Adding a css loader and an error state
    
    * Adding external app test

--- src/test/index.js
+++ src/test/index.js
@@ -1,3 +1,5 @@
+const { generateHtml5Page } = require('../utils');
+
 module.exports = (app) => {
   app.get('/test', (req, res) => {
     res.send(`
@@ -9,4 +11,33 @@ module.exports = (app) => {
     </html>
     `);
   });
+
+  app.get('/test/html5', (req, res) => {
+    const html = generateHtml5Page({
+      title: "TEST_HTML5_TITLE",
+      scriptUrl: "/dist/test.js"
+    });
+
+    res.send(html);
+  });
+
+  app.get('/test/html5bad', (req, res) => {
+    const html = generateHtml5Page({
+      title: "TEST_HTML5_TITLE",
+      scriptUrl: "/error/404/does_not_exist"
+    });
+
+    res.send(html);
+  });
+
+  app.get('/test/html5/external', (req, res) => {
+    const html = generateHtml5Page({
+      title: "TEST_HTML5_TITLE",
+      scriptUrl: "https://alexjpaz.github.io/static/test/test.js"
+    });
+
+    res.send(html);
+
+  });
+
 };
