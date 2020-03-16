2a55b4711bd12e6b5baf1fead0e471aca28acfb1
Alexander Paz <alexjpaz@gmail.com>
2020-03-16 09:53:54 -0400

    Adding check test

--- functions/status-check.js
+++ functions/status-check.js
@@ -1,6 +1,18 @@
+const Mocha = require('mocha');
+
+const mocha = new Mocha();
+
+mocha.addFile(require.resolve('./status-check.test.js'));
+
+mocha.run();
+
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+  const runner = mocha.run((failures) => {
+    console.log(runner);
+
+    callback(null, {
+      statusCode: failures ? 200 : 500,
+      body: "Hello, World"
+    });
   });
 }
