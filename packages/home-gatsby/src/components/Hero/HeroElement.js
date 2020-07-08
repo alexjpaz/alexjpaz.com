1a6fb54cddcbd6e00aa197ef280bed60b648447e
Alexander Paz <alexjpaz@gmail.com>
2020-07-08 09:32:10 -0400

    Adding more links. Bonus dynamic content!

--- packages/home-gatsby/src/components/Hero/HeroElement.js
+++ packages/home-gatsby/src/components/Hero/HeroElement.js
@@ -5,10 +5,27 @@ import { HeroActions } from './HeroActions';
 
 const LinkedInUrl = 'https://www.linkedin.com/in/ajpaz';
 
+export const HeroLink = ({ href, children }) => (
+  <a className='has-text-primary' href={href} target='_blank' rel="noopener noreferrer">{ children }</a>
+);
+
+export class SayAlexPaz extends React.Component {
+  handleClick() {
+    const audio = new Audio();
+    audio.src = 'http://soundboard.alexjpaz.com/alexpaz'
+    audio.play();
+  }
+  render () {
+    return (
+      <span>Most people just call me <a onClick={() => this.handleClick()} className='has-text-primary'>Paz</a>.</span>
+    );
+  }
+}
+
 export const Subtitle = () => (
   <p>
-    I am a full stack <a className='has-text-primary' href={LinkedInUrl}>staff software engineer</a> for <a className='has-text-primary' href='https://realtor.com'>realtor.com</a> with over seven years of experience developing professional software.
-    When I&apos;m not writing tests for my software where I am an avid <a className='has-text-primary' href='/projects/liftit/'>powerlifter</a>, amatuer <a className='has-text-primary' href='https://instagram.com/alexanderthepaz'> photographer</a>, casual <a className='has-text-primary' href='https://www.twitch.tv/alexjpaz'>streamer</a>, and just generally love learning and being <a className='has-text-primary' href='/blogs/pixelart/'>creative</a>.
+    <SayAlexPaz /> I am a full stack <HeroLink href={LinkedInUrl}>staff software engineer</HeroLink> for <HeroLink href='https://realtor.com'>realtor.com</HeroLink> with over seven years of experience developing professional software.
+    When I&apos;m not writing tests for my software where I am an avid <HeroLink className='has-text-primary' href='/projects/liftit/'>powerlifter</HeroLink>, amatuer <HeroLink className='has-text-primary' href='https://instagram.com/alexanderthepaz'> photographer</HeroLink>, casual <HeroLink className='has-text-primary' href='https://www.twitch.tv/alexjpaz'>streamer</HeroLink>, and just generally love learning and being <HeroLink className='has-text-primary' href='/blogs/pixelart/'>creative</HeroLink>.
   </p>
 );
 
@@ -18,7 +35,7 @@ export const HeroElement = () => (
       <div className="container is-narrow">
         <HeroImage />
         <h1 className="title">
-          <a className='has-text-white' href='https://alexjpaz.com/and-his-name-is/Alexander Paz'>Alexander Paz</a>
+          <a className='has-text-white' target='_blank' rel="noopener noreferrer" href='https://alexjpaz.com/and-his-name-is/Alexander Paz'>Alexander Paz</a>
         </h1>
         <h2 className="subtitle">
           <Subtitle />
